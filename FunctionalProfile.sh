51fe2149578b89a8f8c8fa71f9b9ddd9f19e3dde
白洗 <2105028906@qq.com>
2019-09-24 13:07:00 +0800

    Update FunctionalProfile.sh

--- FunctionalProfile.sh
+++ FunctionalProfile.sh
@@ -10,6 +10,11 @@ HuMann2='/home1/Laisenying/miniconda3/bin/humann2'
 export PATH=/share/home1/Laisenying/Tools:$PATH
 for infile in `cat $1`
 do
+
+if
+[ -s "$BasePath/02_rmhuman/${infile}_hg38_paired_clean1.fq.gz" ] && [ -s "$BasePath/02_rmhuman/${infile}_hg38_paired_clean2.fq.gz" ];
+then
+
   cat $BasePath/02_rmhuman/${infile}_hg38_paired_clean1.fq.gz $BasePath/02_rmhuman/${infile}_hg38_paired_clean2.fq.gz > $BasePath/04_combined/${infile}_combined.fq.gz
   $HuMann2 --input $BasePath/04_combined/${infile}_combined.fq.gz \
     --nucleotide-database $chocophlan_database \
@@ -21,4 +26,18 @@ do
     --output-basename ${infile}_humann2 \
     --output $BasePath/humann2_result/${infile}
   rm -rf $BasePath/04_combined/${infile}_combined.fq.gz
+  
+elif
+[ -s "$BasePath/02_rmhuman/${infile}_hg38_paired_clean.fq.gz" ] && [! -f "$BasePath/02_rmhuman/${infile}_hg38_paired_clean1.fq.gz" ];
+then
+  $HuMann2 --input $BasePath/02_rmhuman/${infile}_hg38_paired_clean.fq.gz \
+    --nucleotide-database $chocophlan_database \
+    --protein-database $uniref_database \
+    --verbose \
+    --threads 20 \
+    --remove-temp-output \
+    --taxonomic-profile $BasePath/03_metaphlan2/${infile}.txt \
+    --output-basename ${infile}_humann2 \
+    --output $BasePath/humann2_result/${infile}
+
 done
