a9a412e7fc597807c7deba0ead43f5b783e2dc82
白洗 <2105028906@qq.com>
2019-09-26 11:00:31 +0800

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -5,10 +5,15 @@ TRIMMO_JAR_FILE='/home1/Laisenying/Tools/Trimmomatic-0.39/trimmomatic-0.39.jar'
 TRIMMO_ADAPTOR_FILE_PE='/home1/Laisenying/Tools/Trimmomatic-0.39/adapters/TruSeq3-PE.fa'
 Bowtie2='/home1/Laisenying/miniconda3/bin/bowtie2'
 BasePath='/home1/Laisenying/Data-analysis/CRC'
+Human_database='/home1/Laisenying/Tools/data/human/hg3'
 
 for infile in `cat $1`
 do
-# 序列质控
+
+if
+[ -s "$BasePath/00_rawdata/${infile}/${infile}_1.fastq.gz" ] && [ -s "$BasePath/00_rawdata/${infile}/${infile}_2.fastq.gz" ];
+then
+
 /usr/bin/java -jar $TRIMMO_JAR_FILE PE \
     -threads 8 \
     $BasePath/00_rawdata/${infile}/${infile}_1.fastq.gz  \
@@ -23,7 +28,7 @@ rm -rf $BasePath/01_trim/${infile}_clean_unpaired.1.fq.gz
 rm -rf $BasePath/01_trim/${infile}_clean_unpaired.2.fq.gz
 
 # 去宿主序列
-$Bowtie2 -x /home1/Laisenying/Tools/data/human/hg38  \
+$Bowtie2 -x $Human_database  \
     -p 8 \
     --very-sensitive \
     -1 $BasePath/01_trim/${infile}_clean.1.fq.gz \
@@ -33,4 +38,46 @@ $Bowtie2 -x /home1/Laisenying/Tools/data/human/hg38  \
     
 rm -rf $BasePath/02_rmhuman/${infile}_hg38_paired_contam1.fq.gz 
 rm -rf $BasePath/02_rmhuman/${infile}_hg38_paired_contam2.fq.gz 
+
+elif
+[ -s "$BasePath/00_rawdata/${infile}/${infile}.fastq.gz" ] && [! -f "$BasePath/00_rawdata/${infile}/${infile}_1.fastq.gz" ];
+then
+
+/usr/bin/java -jar $TRIMMO_JAR_FILE SE \
+    -threads 8 \
+    $BasePath/00_rawdata/${infile}/${infile}.fastq.gz  \
+    $BasePath/01_trim/${infile}_clean.fq.gz \
+    $BasePath/01_trim/${infile}_clean_unpaired.fq.gz \
+    ILLUMINACLIP:$TRIMMO_ADAPTOR_FILE_PE:2:15:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:15:30 MINLEN:50
+
+rm -rf $BasePath/01_trim/${infile}_clean_unpaired.fq.gz
+
+# 去宿主序列
+$Bowtie2 -x $Human_database  \
+    -p 8 \
+    --very-sensitive \
+    -U $BasePath/01_trim/${infile}_clean.fq.gz \
+    --un-conc-gz $BasePath/02_rmhuman/${infile}_hg38_paired_clean.fq.gz \
+    --al-conc-gz $BasePath/02_rmhuman/${infile}_hg38_paired_contam.fq.gz 
+    
+rm -rf $BasePath/02_rmhuman/${infile}_hg38_paired_contam.fq.gz 
+fi
+
+# remove file in 00_rawdata and 01_trim to save space
+if
+[ -s "$BasePath/02_rmhuman/${infile}_hg38_paired_clean.fq.gz" ];
+then
+rm -rf $BasePath/01_trim/${infile}_clean.fq.gz
+rm -rf $BasePath/00_rawdata/${infile}/${infile}.fastq.gz
+elif
+[-s "$BasePath/02_rmhuman/${infile}_hg38_paired_clean1.fq.gz"] && [-s "$BasePath/02_rmhuman/${infile}_hg38_paired_clean2.fq.gz"];
+then
+rm -rf $BasePath/01_trim/${infile}_clean.1.fq.gz
+rm -rf $BasePath/01_trim/${infile}_clean.2.fq.gz
+rm -rf $BasePath/00_rawdata/${infile}/${infile}_1.fastq.gz
+rm -rf $BasePath/00_rawdata/${infile}/${infile}_2.fastq.gz
+fi
+
 done
+
+
